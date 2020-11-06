8c42b9ff5393af96ea71bcff3fe399464b4a49b3
Linda <stayathomegeek@gmail.com>
2020-11-06 14:11:40 -0500

    Added a new view, dummied up right now, that will loop through all blogs

--- myblogs/views.py
+++ myblogs/views.py
@@ -100,3 +100,52 @@ def count_words_improved(request):
         feed_html="none"
         
     return render(request, 'myblogs/count_words.html', {'feed_html': feed_html})     
+
+#########################################
+#########################################
+#########################################
+def count_words_all_blogs(request):
+    '''
+    # This improved version of the above view will use a better cue for beautifulsoup
+    # I found that all of the blog posts have the word "post-body-" followed by a number
+    # That's what I will use
+    # get the recipes from the RSS feed
+    # loop through each recipe and count its words.
+    # If word count is less than a hundred, store the recipe in a dictionary
+    # create a list of all the dictionaries
+    # I figured out that I could use selenium for post-body-<followed by post num> because selenium lets you do "contains."
+    ''' 
+    
+    
+    feed = (feedparser.parse(
+        'https://thecattycook.blogspot.com/feeds/posts/default?start-index=1&max-results=1000'))
+    feed_html =""
+    newfeed = list(feed.entries)
+    options = webdriver.ChromeOptions()       
+    options.add_argument("--headless")         
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-features=NetworkService")
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--disable-features=VizDisplayCompositor")             
+    options.add_argument('--no-sandbox') # This is to prevent selenium webdriver chrome Message: unknown error: unable to discover open pages   
+    options.add_argument("headless")   
+    driver = webdriver.Chrome(options=options, executable_path="C:\\Users\\Linda\\Dev\\blogger_health\\myblogs\\chromedriver.exe") 
+    
+    for i, post in enumerate(newfeed):    # Traverse through all the posts in the blog        
+         
+        i = i + 1 
+        url = post.link
+        driver.get(url)          
+        result=driver.find_element(By.XPATH, '//*[contains(@id, "post-body-")]')           
+        the_length = len(result.text)     
+        
+        if the_length < 300:             
+            if post.title == "":  # we need to put a placeholder in so it's easy to understand that there was no title
+               post.title = "NO TITLE"        
+            feed_html = feed_html + "<a href=" + post.link + ">" + post.title + "</a>" + " " + str(the_length) + "<br>"  
+                
+    driver.quit()    
+    if not feed_html:
+        feed_html="none"
+    return render(request, 'myblogs/count_words_all_blogs.html', {'feed_html': feed_html})          
