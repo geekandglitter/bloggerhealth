fe912b54fe3066e5ef5d6cdec91a6e82baa84883
Linda <stayathomegeek@gmail.com>
2020-11-07 07:22:16 -0500

    Added this line but I don't know what it does: urlpatterns += staticfiles_urlpatterns()

--- web_project/urls.py
+++ web_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", include("myblogs.urls")),
 ]
+urlpatterns += staticfiles_urlpatterns()
